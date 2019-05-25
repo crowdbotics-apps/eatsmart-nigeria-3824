b07f56cfbdc01ff8c50b62728d0041fbf268a97c
Crowdbotics <team@crowdbotics.com>
2019-05-25 14:08:00 +0000

    initial commit

---
+++ android/app/src/main/java/com/eatsmart_nigeria_3824/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eatsmart_nigeria_3824;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "eatsmart_nigeria_3824";
+    }
+}
